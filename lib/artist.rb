c001e7ee62617bd329444fe554e72c09dc0077cf
Saint-Jean Germila <germila@noukod.com>
2018-10-10 18:26:09 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,29 @@
 class Artist
-attr_accessor :name
 
-  def initilize(name)
-    @name = name
+  attr_accessor :name,:songs
+
+  @@songs=[]
+
+  def initialize(name)
+    @name=name
+    @songs=[]
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist=self
+    @@songs << song
+  end
+
+  def add_song_by_name(name)
+   song = Song.new(name)
+   songs << song
+   @@songs << song
+   song.artist = self
+  end
+
+  def self.song_count
+    @@songs.count
+  end
+
 end
