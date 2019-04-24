17d4a574465b561c25e4933a1702cc053b12b7a1
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-24 19:13:59 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ 
+  def add_song(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+ 
+  def songs
+    @songs
+  end
+end
